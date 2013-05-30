3e32c1142332abe2189ab9505a5ed6922b58fc39
siekiera <mistrzsiekiera@gmail.com>
2013-05-30 16:34:01 +0200

    receivery

---
+++ src/elka/clouddir/client/ServerReceiver.java
@@ -0,0 +1,12 @@
+package elka.clouddir.client;
+
+/**
+ * Wątek komunikujący się z serwerem
+ */
+public class ServerReceiver implements Runnable {
+
+    @Override
+    public void run() {
+        //TODO
+    }
+}
---
+++ src/elka/clouddir/server/ClientReceiver.java
@@ -0,0 +1,13 @@
+package elka.clouddir.server;
+
+/**
+ * Wątek odpowiadający za komunikację z klientem
+ */
+public class ClientReceiver implements Runnable
+{
+    @Override
+    public void run()
+    {
+        //TODO
+    }
+}
