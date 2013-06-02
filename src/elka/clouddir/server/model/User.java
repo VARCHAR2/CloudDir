4617d8b6a57f11d65bd63fce0977f86860aa4b57
Lukasz Pielaszek <loky91@gmail.com>
2013-06-02 12:32:50 +0200

    Add serialization to file for User and UserGroup

--- src/elka/clouddir/server/model/User.java
+++ src/elka/clouddir/server/model/User.java
@@ -1,21 +1,27 @@
 package elka.clouddir.server.model;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
-public class User implements Serializable{
-	private static final long serialVersionUID = 1L;
+import elka.clouddir.shared.FilesMetadata;
+
+public class User implements Serializable {
 	private String name;
-    private String password;
+	private String password;
 	private boolean loggedIn;
 	private UserGroup userGroup;
-	
-	
-	public User(String name, boolean loggedIn, UserGroup userGroup, String password) {
+
+	public User(String name, boolean loggedIn, UserGroup userGroup,
+			String password) {
 		super();
 		this.name = name;
 		this.loggedIn = loggedIn;
-        this.password = password;
-        this.setUserGroup(userGroup);
+		this.password = password;
+		this.setUserGroup(userGroup);
 	}
 
 	public String getName() {
@@ -42,11 +48,66 @@ public class User implements Serializable{
 		this.userGroup = userGroup;
 	}
 
-    public String getPassword() {
-        return password;
-    }
+	public String getPassword() {
+		return password;
+	}
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	/**
+	 * Saves the state of the object to file identified by the {@code name}.
+	 */
+	public void pushToFile() {
+		try (FileOutputStream fileOut = new FileOutputStream("ser/user-" + name
+				+ ".ser");
+				ObjectOutputStream out = new ObjectOutputStream(fileOut);) {
+			out.writeObject(this);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Reads the state of the object from file. 
+	 * You must be sure of what you are doing if you want to use this method.
+	 * Better use public void pullFromFile(String userName).
+	 */
+	public void pullFromFile() {
+		try (FileInputStream fileIn = new FileInputStream("ser/user-" + name + ".ser");
+				ObjectInputStream in = new ObjectInputStream(fileIn);) {
+			User u = (User) in.readObject();
+			this.loggedIn = false;
+			this.name = u.name;
+			this.password = u.password;
+			this.userGroup = u.userGroup;
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Reads the state of the object from file.
+	 */
+	public void pullFromFile(String userName) {
+		try (FileInputStream fileIn = new FileInputStream("ser/user-"
+				+ userName + ".ser");
+				ObjectInputStream in = new ObjectInputStream(fileIn);) {
+			User u = (User) in.readObject();
+			this.loggedIn = false;
+			this.name = u.name;
+			this.password = u.password;
+			this.userGroup = u.userGroup;
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
 }
--- src/elka/clouddir/server/model/UserGroup.java
+++ src/elka/clouddir/server/model/UserGroup.java
@@ -1,20 +1,20 @@
 package elka.clouddir.server.model;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public class UserGroup {
+public class UserGroup implements Serializable{
 	private String name;
-	private Path sharedFolderPath;
+	private String sharedFolderPath;
 	
 
 	public UserGroup(String name, String sharedFolderPath) {
-		super();
-		this.name = name;
-		this.sharedFolderPath = Paths.get(sharedFolderPath);
-	}
-	
-	public UserGroup(String name, Path sharedFolderPath) {
 		super();
 		this.name = name;
 		this.sharedFolderPath = sharedFolderPath;
@@ -28,12 +28,62 @@ public class UserGroup {
 		this.name = name;
 	}
 	
-	public Path getSharedFolderPath() {
+	public String getSharedFolderPath() {
 		return sharedFolderPath;
 	}
 	
-	public void setSharedFolderPath(Path sharedFolderPath) {
+	public void setSharedFolderPath(String sharedFolderPath) {
 		this.sharedFolderPath = sharedFolderPath;
+	}
+	
+	/**
+	 * Saves the state of the object to file identified by the {@code name}.
+	 */
+	public void pushToFile() {
+		try (FileOutputStream fileOut = new FileOutputStream("ser/group-" + name
+				+ ".ser");
+				ObjectOutputStream out = new ObjectOutputStream(fileOut);) {
+			out.writeObject(this);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Reads the state of the object from file. 
+	 * You must be sure of what you are doing if you want to use this method.
+	 * Better use public void pullFromFile(String userName).
+	 */
+	public void pullFromFile() {
+		try (FileInputStream fileIn = new FileInputStream("ser/group-" + name + ".ser");
+				ObjectInputStream in = new ObjectInputStream(fileIn);) {
+			UserGroup u = (UserGroup) in.readObject();
+			this.name = u.name;
+			this.sharedFolderPath = u.sharedFolderPath;
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Reads the state of the object from file.
+	 */
+	public void pullFromFile(String groupName) {
+		try (FileInputStream fileIn = new FileInputStream("ser/group-"
+				+ groupName + ".ser");
+				ObjectInputStream in = new ObjectInputStream(fileIn);) {
+			UserGroup u = (UserGroup) in.readObject();
+			this.name = u.name;
+			this.sharedFolderPath = u.sharedFolderPath;
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	
